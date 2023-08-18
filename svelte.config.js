8ad18c70676c61025dfac579376854244c2c6f18
Keno Bohlen <tonyflow90@gmail.com>
2023-08-19 00:27:19 +0200

    u2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import {vitePreprocess} from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
   kit: {
@@ -7,6 +9,10 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    paths: {
+      base:
+        process.env.NODE_ENV === "production" ? "/sveltekit-github-pages" : "",
+    },
     alias: {
       $components: "src/lib/components",
       "$components/*": "src/lib/components/*",
@@ -14,11 +20,11 @@ const config = {
       "$stores/*": "src/lib/stores/*",
       $themes: "static/theme",
       "$themes/*": "static/theme/*",
-    }
+    },
   },
   preprocess: [vitePreprocess({})],
   shadcn: {
-    componentPath: './src/lib/components/ui'
-  }
+    componentPath: "./src/lib/components/ui",
+  },
 };
 export default config;
