e9425c8bfbeaa09c487c42a6fd65d147f6c731aa
Keno Bohlen <tonyflow90@gmail.com>
2023-08-19 00:38:44 +0200

    u3

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,6 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    paths: {
-      base:
-        process.env.NODE_ENV === "production" ? "/build" : "",
-    },
     alias: {
       $components: "src/lib/components",
       "$components/*": "src/lib/components/*",
